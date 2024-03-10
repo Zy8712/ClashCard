d5b2aee461e24d16c0bcfdb28b85e534d94bbf1b
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2024-03-10 03:52:16 +0000

    Added styling for a small portion of the Generate page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import NavOptions from "./navbar-components/NavOptions";
 export default function Navbar(){
     return(
         <>
-            <div className="absolute z-50 w-full h-24 bg-white bg-opacity-25 px-14">
+            <div className="absolute z-50 top-0 w-full h-24 bg-white bg-opacity-25 px-14">
                 <div className="w-full h-full flex justify-between items-center">
                     <Logo />
 
--- src/pages/Generate.jsx
+++ src/pages/Generate.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import SearchIcon from "../assets/icons/magnifying-lens-svgrepo-com.svg"
 
 const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFkZGZmZDAwLTc0ZjItNDUwNS1iNDQ5LWQxZjllYWFmYTQ3YSIsImlhdCI6MTcwOTI3MDM1NCwic3ViIjoiZGV2ZWxvcGVyLzVhYmJlNDg5LTc3NjItYTU3YS0zMDkzLWFiNGJlMzMxNDU2MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NS43OS4yMTguNzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.JkAfYAoVCHpdNpv306bxVP4pkAv1QyC8GFASGc36gYh7EAlhX9IVvqvAml0cBqaijdZ3cCpvL27weP3YkNe5zg'; // Your Clash Royale API key
 const PROXY_URL = 'https://proxy.royaleapi.dev/v1/'; // RoyaleAPI proxy URL
@@ -46,24 +47,62 @@ export default function Generate() {
     };
 
     return (
-        <div className='w-screen h-screen border-2 border-blue-500 border-solid'>
+        <div className='w-full h-full min-h-screen flex flex-col items-center px-14 pt-32 pb-14'>
 
-            <div className="mt-24 bg-gray-400">
-                <h1 className=''>Search for a Player by Player Tag</h1>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" value={playerTag} onChange={handleInputChange} />
-                    <button type="submit">Search</button>
-                </form>
-                {loading && <div>Loading...</div>}
-                {error && <div>Error: {error}</div>}
-                {playerData && (
+            <h1 className="text-center text-white text-6xl font-theme-orbitron font-semibold">Profile Card Generator</h1>
+
+            <div className="h-12 mt-8 flex items-center font-theme-oxanium bg-gray-500 border-white border-solid border-4 rounded-full ps-2 pe-4">
+
+                <div className='w-64 h-full flex justify-center items-center'>
+                    <h2 className="text-white text-lg font-semibold">Search for a Player Profile:</h2>
+                </div>
+
+                <div className="w-80 bg-white rounded-md overflow-hidden">
+                    <form onSubmit={handleSubmit} className="flex justify-between items-center ps-2">
+                        <input type="text"
+                            value={playerTag}
+                            onChange={handleInputChange}
+                            placeholder="Enter Player Tag (e.g. #12345678)"
+                            className="w-64 h-7 rounded-md outline-none"
+                        />
+                        <button type="submit" title="Search" className="w-10 h-7 flex justify-center items-center bg-violet-400">
+                            <img src={SearchIcon} className="w-6" />
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+
+            <div className="mt-6 w-full flex flex-col items-center ">
+
+                {loading &&
                     <div>
+                        Loading...
+                    </div>
+                }
+
+                {error &&
+                    <div>
+                        Error: {error}
+                    </div>
+                }
+
+                {playerData && (
+                    <div className="w-[850px] h-[420px] bg-white border-black border-2 border-solid">
                         <h2>Player Information</h2>
                         <p>Name: {playerData.name}</p>
                         <p>Tag: {playerData.tag}</p>
+                        <p>Exp Lvl: {playerData.expLevel}</p>
+                        <p>Trophies: {playerData.trophies}</p>
+                        <p>PB Trophies: {playerData.bestTrophies}</p>
+                        <p>Wins: {playerData.wins}</p>
+                        <p>Loses: {playerData.losses}</p>
+                        <p>BattleCount: {playerData.battleCount}</p>
+                        <p>3 Crown Wins: {playerData.threeCrownWins}</p>
                         {/* Add more player data fields here */}
                     </div>
                 )}
+
             </div>
 
         </div>
